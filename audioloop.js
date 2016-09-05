ff19d92c79e5031ebe7ff7879d02132792f28747
Yoshiyuki Koyanagi <moutend@gmail.com>
2016-09-05 10:07:42 +0900

    Add current sample rate as playbackSample in callback arguments

--- audioloop.js
+++ audioloop.js
@@ -1,12 +1,14 @@
 "use strict";
 module.exports = (channels, blockSize, sampleRate, callback) => {
   let currentTime = Date.now() * 0.001;
+  let currentSample = 0;
   const loop = () => {
-    // 0.1秒以上先はレンダリングしない
     if (currentTime < (Date.now() * 0.001) + 0.1) {
       const output = new Float32Array(channels * blockSize);
-      callback(output, currentTime);
+      callback(output, currentTime, currentSample);
       currentTime += blockSize / sampleRate;
+      currentSample += blockSize;
+      currentSample = currentSample % sampleRate;
       const buffer = Buffer.from(output.buffer);
       // Buffer.from がエラーになる場合は new Buffer(output.buffer) とする
       // 書き込みバッファがいっぱいになったら 'drain' (書き込み再開可能) まで待機
--- noise.js
+++ noise.js
@@ -1,8 +1,7 @@
 "use strict";
 const audioloop = require("./audioloop");
 // channels, blockSize, sampleRate, callback
-audioloop(1, 128, 44100, (output, playbackTime) => {
-  // ここで信号処理をする
+audioloop(1, 128, 44100, (output, playbackTime, playbackSample) => {
   for (let i = 0; i < output.length; i++) {
     output[i] = Math.random() * Math.sin(playbackTime * 0.250);
   }
